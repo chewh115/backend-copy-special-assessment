2177b15501614a42b1944ecf8797815ca8d8a9bb
Chelsea <chewh115@gmail.com>
2020-03-05 15:35:21 -0500

    Updates to have working solutions and cleans up code

--- copyspecial.py
+++ copyspecial.py
@@ -19,8 +19,8 @@ import argparse
 # This is to help coaches and graders identify student assignments
 __author__ = """chewh115, with the aid of Geeks for Geeks and
                 Google's own os documentation"""
-if sys.version_info[0] < 3:
-    raise Exception('Hello! Please use python3!')
+# if sys.version_info[0] < 3:
+#     raise Exception('Hello! Please use python3!')
 
 
 def get_special_paths(fromdir):
@@ -42,43 +42,35 @@ def copy_to(paths, todir):
     print(todir)
     for path in paths:
         shutil.copy(path, todir)
+        print(f"Copied {path} to {todir}")
 
 
 def zip_to(paths, zippath):
     """Given a list of paths, zip those files into specified zipfile"""
     command = 'zip -j {} {}'.format(zippath, ' '.join(paths))
-    print(f"Command I'm going to do {command}")
+    print("Command I'm going to do: {}".format(command))
     subprocess.run(command)
 
 
 def main():
-    # This snippet will help you get started with the argparse module.
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
     parser.add_argument('fromdir', help='dir to search for special files')
     args = parser.parse_args()
 
-    # TODO you must write your own code to get the cmdline args.
-    # Read the docs and examples for the argparse module about how to do this.
-
-    # Parsing command line arguments is a must-have skill.
-    # This is input data validation.  If something is wrong (or missing)
-    # with any required args, the general rule is to
-    # print a usage message and exit(1).
-
-    # +++your code here+++
     if not args:
         parser.print_usage()
         sys.exit(1)
 
-    paths = get_special_paths(fromdir)
-    if --todir:
-        copy_to(paths, todir)
-    elif --tozip:
-        zip_to(paths, tozip)
+    paths = get_special_paths(args.fromdir)
+    if args.todir:
+        copy_to(paths, args.todir)
+    elif args.tozip:
+        zip_to(paths, args.tozip)
     else:
-        print(paths)
+        for file in paths:
+            print(file)
 
 
 if __name__ == "__main__":
